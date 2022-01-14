ec98ac4e57567c14109c3f2fc7c639758255fc6a
Obada Khalili <obada.akhalili@gmail.com>
2022-01-14 11:19:12 +0200

    Knowledge(houses): refine

--- modules/Knowledge/houses.py
+++ modules/Knowledge/houses.py
@@ -1,14 +1,14 @@
 from logic import Self, Not, And, Or, XOr, Implification, model_checking
 
+
+def format_character_in_house(character, house):
+    return f"{character} belongs to {house}"
+
+
 if __name__ == "__main__":
     try:
         characters = ["Gilderoy", "Minerva", "Pomona", "Horace"]
         houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-
-        symbols = {
-            character: {house: f"{character} belongs to {house}" for house in houses}
-            for character in characters
-        }
 
         KB = And()
 
@@ -17,41 +17,49 @@ if __name__ == "__main__":
         to a different one of the four houses: Gryffindor,
         Hufflepuff, Ravenclaw, and Slytherin House.
         """
-        for character_i in symbols:
-            one_house_each = XOr()
+        for character_i in characters:
+            one_house_per_char = XOr()
 
-            for house in symbols[character_i]:
-                one_house_each.add(symbols[character_i][house])
+            for house in houses:
+                one_house_per_char.add(format_character_in_house(character_i, house))
 
-                if_char_in_house = Implification(symbols[character_i][house])
+                one_char_per_house = Implification(
+                    format_character_in_house(character_i, house)
+                )
                 chars_not_in_house = And()
 
-                for character_j in symbols:
+                for character_j in characters:
                     if character_j != character_i:
-                        chars_not_in_house.add(Not(symbols[character_j][house]))
+                        chars_not_in_house.add(
+                            Not(format_character_in_house(character_j, house))
+                        )
 
-                KB.add(if_char_in_house.add(chars_not_in_house))
+                KB.add(one_char_per_house.add(chars_not_in_house))
 
-            KB.add(one_house_each)
+            KB.add(one_house_per_char)
 
         """
         Gilderoy belongs to Gryffindor or Ravenclaw
         """
-        KB.add(Or(symbols["Gilderoy"]["Gryffindor"], symbols["Gilderoy"]["Ravenclaw"]))
+        KB.add(
+            Or(
+                format_character_in_house("Gilderoy", "Gryffindor"),
+                format_character_in_house("Gilderoy", "Ravenclaw"),
+            )
+        )
 
         """
         Pomona does not belong in Slytherin.
         """
-        KB.add(Not(symbols["Pomona"]["Slytherin"]))
+        KB.add(Not(format_character_in_house("Pomona", "Slytherin")))
 
         """
         Minerva belongs to Gryffindor.
         """
-        KB.add(symbols["Minerva"]["Gryffindor"])
+        KB.add(format_character_in_house("Minerva", "Gryffindor"))
 
-        for character in symbols:
-            for house in symbols[character]:
-                if model_checking(KB, Self(symbols[character][house])):
-                    print(symbols[character][house])
+        for symbol in KB.symbols():
+            if model_checking(KB, Self(symbol)):
+                print(symbol)
     except RuntimeError as e:
         print(e)
