904b1ed22c3505af40c40809f53190e979b21673
Obada Khalili <obada.akhalili@gmail.com>
2022-07-01 11:23:49 +0300

    rename some files to reflect their program intention and not the implemented algorithms, and move backtracking implementation to its own file

--- modules/Optimization/exam_scheduling.py
+++ modules/Optimization/exam_scheduling.py
@@ -1,26 +1,4 @@
-class SolutionDNE(RuntimeError):
-    def __init__(self):
-        super().__init__("solution does not exist")
-
-
-def backtrack(csp, assignments={}):
-    unassigned_vars = csp.vars - assignments.keys()
-    is_assignment_complete = not bool(unassigned_vars)
-
-    if is_assignment_complete:
-        return assignments
-
-    var = unassigned_vars.pop()
-
-    for value in csp.get_domain_of_var(var):
-        if csp.is_assignment_consistent(var, value, assignments):
-            assignments[var] = value
-            try:
-                return backtrack(csp, assignments)
-            except SolutionDNE:
-                del assignments[var]
-
-    raise SolutionDNE()
+import backtracking
 
 
 class ExamSchedulingCSP:
@@ -65,6 +43,6 @@ class ExamSchedulingCSP:
 
 
 try:
-    print(backtrack(ExamSchedulingCSP()))
-except SolutionDNE as e:
+    print(backtracking.backtrack(ExamSchedulingCSP()))
+except backtracking.SolutionDNE as e:
     print(e)
